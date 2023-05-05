8b963d5b7837aa0f2903881fc277559c847f2775
Uchenna Ofor <ucheofor2015@gmail.com>
2023-05-05 07:47:24 +0100

    .gitignore updated

--- telecom/settings.py
+++ telecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5)h^bxa&)!maa8^9wj+c8ys90x!qek+2m+e5c5*-(abyc*@#n#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['veepay.onrender.com', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
@@ -107,7 +107,6 @@ import environ
 
 env = environ.Env()
 env.read_env()
-
 
 ## Render.com postgres service LIVE
 import dj_database_url
