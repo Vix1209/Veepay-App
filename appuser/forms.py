3303dd384996f994dd0f0e422ecd75f1aca2fca6
Uchenna Ofor <ucheofor2015@gmail.com>
2023-06-10 15:19:48 +0100

    profile update form created and set up

--- appuser/forms.py
+++ appuser/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class SignUpForm (UserCreationForm):
@@ -81,3 +82,14 @@ class SignUpForm (UserCreationForm):
 #         fields = ('username', 'password')
 
     
+    
+class UserUpdateForm (forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','first_name', 'last_name', 'email' ]
+        
+
+class ProfileUpdateForm (forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone', 'image']
